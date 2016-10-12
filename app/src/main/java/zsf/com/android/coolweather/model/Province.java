bbaeb3a61b4ea695fecec3634c8db190e0b86961
zhangsf <zhangsf0707@thundersoft.com>
2016-10-12 15:41:03 +0800

    新增数据库帮助类,以及各表对应的实体类

---
+++ app/src/main/java/zsf/com/android/coolweather/model/Province.java
@@ -0,0 +1,35 @@
+package zsf.com.android.coolweather.model;
+
+/**
+ * Created by zhangsf on 16-10-12.
+ */
+
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
